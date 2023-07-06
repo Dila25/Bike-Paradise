5905f36173516062c2dcdcf0182e5bca5643b1fd
N.G.Dilshan Priyawansha <99160680+Dila25@users.noreply.github.com>
2023-07-06 14:06:24 +0530

    File Uplode

---
+++ Bike.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "RC";
+}
+
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = "DUKATI";
+}
+
+function myFunction3() {
+    document.getElementById("demo3").innerHTML = "KTM";
+}
+
+function myFunction4() {
+    document.getElementById("demo4").innerHTML = "R6";
+}
+
+function myFunction5() {
+    document.getElementById("demo5").innerHTML = "S";
+}
+
+function myFunction6() {
+    document.getElementById("demo6").innerHTML = "R1";
+}
